33aebf9e7b9abbbaccc453a442ae138fe05d43f6
Thiago Santos <47781320+thiagoolvr@users.noreply.github.com>
2023-10-19 15:32:50 -0300

    config gpio

--- main.c
+++ main.c
@@ -3,7 +3,35 @@
 /**
  * main.c
  */
-int main(void)
-{
-	return 0;
+
+#include "F28x_Project.h"
+
+void ConfigureGpio(void);
+
+void Init(void) {
+    ConfigureGpio();
+}
+
+int main(void) {
+	InitSysCtrl();
+
+	Init();
+
+	while(1) {
+	    GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;
+
+	    if (GPIO_ReadPin(59) == 1)
+	        GPIO_WritePin(34, true);  // LED RED OFF
+	    else
+	        GPIO_WritePin(34, false); // LED RED ON
+	}
+}
+
+void ConfigureGpio(void) {
+    InitGpio();
+    EALLOW;
+    GPIO_SetupPinOptions(31, GPIO_OUTPUT, 0);
+    GPIO_SetupPinOptions(34, GPIO_OUTPUT, 0);
+    GPIO_SetupPinOptions(59, GPIO_INPUT, GPIO_PULLUP);
+    EDIS;
 }
